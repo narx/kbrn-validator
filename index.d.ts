d1940b863b8b2156489232b233dcfd40d5929286
narx17 <narx17@gmail.com>
2017-08-31 15:47:22 +0900

    typing

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,18 @@
-
-export as namespace kbrnValidator;
-
-export interface ValidateResult {
-    isValid: boolean;
-    message: string;
-    information?: Information;
-}
-
-export interface Information {
-    taxOfficeCode: string;
-    typeCode: string;
-    type: string;
-    detailType: string;
-    isTaxFree : boolean;
-    taxType: string;
-}
-
-export function validate(kbrn: string): ValidateResult;
-
+declare module 'kbrn-validator' {
+    namespace KbrnValidator {
+        interface ValidateResult {
+            isValid: boolean;
+            message: string;
+            information?: Information;
+        }
+        interface Information {
+            taxOfficeCode: string;
+            typeCode: string;
+            type: string;
+            detailType: string;
+            isTaxFree : boolean;
+            taxType: string;
+        }
+        function validate(kbrn: string): ValidateResult;
+    }
+}   
